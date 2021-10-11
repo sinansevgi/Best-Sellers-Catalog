11d54110e8495b45d9dbc04283370c87ec9bb412
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-10-12 00:22:37 +0300

    Initialize react app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../assets/App.css';
+import Navbar from './Navbar';
+import Routes from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;

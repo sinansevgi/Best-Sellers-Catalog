8c19ff5a8002d2db270aee76481a455230a7d026
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-10-12 20:12:58 +0300

    Add basic css styling

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import detailsStyle from '../assets/Details.module.css';
 
 const Details = ({ book }) => {
   if (!book) {
@@ -12,13 +13,18 @@ const Details = ({ book }) => {
     );
   }
   return (
-    <div>
+    <div className={detailsStyle.details}>
       <h2>{book.title}</h2>
       <img src={`http://covers.openlibrary.org/b/isbn/${book.primary_isbn13}-L.jpg`} alt="Book Cover" />
-      <h3>
-        {`Author : ${book.author}`}
-      </h3>
-      <p>{`Description : ${book.description}`}</p>
+      <div className={detailsStyle.container}>
+        <h2>Author</h2>
+        <h3>{book.author}</h3>
+      </div>
+      <div className={detailsStyle.container}>
+        <h2>Description</h2>
+        <p>{book.description}</p>
+      </div>
+
     </div>
   );
 };
